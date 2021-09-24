2523223a958405427c5082725f53d4ded800fc04
Lucas D. Soares <lsoares@itelinfo.com>
2021-09-24 11:19:34 -0400

    Primeiro Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarPessoaComponent } from './listar-pessoa/listar-pessoa.component';
 import { ListarAtletaComponent } from './listar-atleta/listar-atleta.component';
+import { ListarProvinciaComponent } from './listar-provincia/listar-provincia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListarPessoaComponent,
-    ListarAtletaComponent
+    ListarAtletaComponent,
+    ListarProvinciaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/listar-provincia/listar-provincia.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ProvinciaService } from '../provincia.service';
+import { Provincia } from '../entities/provincia';
+
+@Component({
+  selector: 'app-listar-provincia',
+  templateUrl: './listar-provincia.component.html',
+  styleUrls: ['./listar-provincia.component.css']
+})
+export class ListarProvinciaComponent implements OnInit {
+
+  provincias: ProvinciaService[] = [];
+
+  constructor( private servicoProvincia: ProvinciaService ) { }
+
+  ngOnInit(): void {
+
+    this.servicoProvincia.listar().subscribe(
+      data=> {this.provincias = data},
+      error => console.log("Erro ao listar atleta", error)
+    )
+  }
+
+}
